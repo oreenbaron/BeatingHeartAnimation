699986b5844932a1ac06f63c6e6115d4cad57ce4
Oreen Baron <orinbaron@orins-mbp.home>
2018-05-23 17:17:02 +0300

    Beating Heart animation created

--- HeartAnimation/ViewController.swift
+++ HeartAnimation/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heartButton: UIButton!
+    @IBOutlet weak var pulseUIVew: UIView!
+    var heartBeating = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func heartPressed(_ sender: UIButton) {
+        
+        heartBeating = !heartBeating
+        
+            pulsate()
 
+    }
+    
+    func pulsate() {
+        
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        
+        pulse.duration = 1.5
+        pulse.fromValue = 0.95
+        pulse.toValue = 1.2
+        pulse.autoreverses = true
+        pulse.repeatCount = .greatestFiniteMagnitude
+        pulse.initialVelocity = 0.95
+        pulse.damping = 1.0
+        
+        if !heartBeating {
+            
+            self.heartButton.layer.add(pulse, forKey: "pulse")
+            
+        } else if heartBeating {
+            
+            self.heartButton.layer.removeAllAnimations()
+        }
+    }
 }
+
 
